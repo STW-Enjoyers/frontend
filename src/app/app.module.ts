2c27b6238e1b5f91359e953255e92f52487695b1
diego <755232@unizar.es>
2022-04-02 01:10:51 +0200

    Add header placeholder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HomeComponent} from "./pages/home/home.component";
 import {ButtonComponent} from "./components/button/button.component";
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ButtonComponent
+    ButtonComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
