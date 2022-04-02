70e9928bb96f110fdb41be240c73ae6aa0e688d0
diego <755232@unizar.es>
2022-04-02 02:09:23 +0200

    Refactor home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import {HomeComponent} from "./pages/home/home.component";
 import {ButtonComponent} from "./components/button/button.component";
 import { HeaderComponent } from './components/header/header.component';
+import { FiltradorNotasComponent } from './pages/filtrador-notas/filtrador-notas.component';
+import { ListaCarrerasComponent } from './pages/lista-carreras/lista-carreras.component';
+import { MapaErasmusComponent } from './pages/mapa-erasmus/mapa-erasmus.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ButtonComponent,
-    HeaderComponent
+    HeaderComponent,
+    FiltradorNotasComponent,
+    ListaCarrerasComponent,
+    MapaErasmusComponent
   ],
   imports: [
     BrowserModule,
