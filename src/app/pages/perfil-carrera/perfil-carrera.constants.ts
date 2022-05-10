314da74b7f45b9e5fceed5fd07ab1e1f5cd73358
diego <755232@unizar.es>
2022-05-10 20:58:01 +0200

    Adjusted comment pagination

--- src/app/pages/perfil-carrera/perfil-carrera.constants.ts
+++ src/app/pages/perfil-carrera/perfil-carrera.constants.ts
@@ -4,7 +4,7 @@ export const COMMENT_INPUT_LABEL:string = "Deja tu comentario, consejo u opinió
 export const COMMENT_BUTTON_TEXT:string = "Publicar";
 export const OPTION_RELEVANCE:string = "comentarios destacados";
 export const OPTION_DATE:string = "comentarios más recientes";
-export const MAX_COMMENTS_PER_PAGE:number = 10;
+export const MAX_COMMENTS_PER_PAGE:number = 5;
 
 
 export const CHART_PIE_LABELS = [ "Graduados", "Cambio de carrera", "Abandono"];
