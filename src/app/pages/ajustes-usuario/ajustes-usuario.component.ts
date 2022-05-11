a422810e006fc9e9d19753a6b05bcb647a3a6927
diego <755232@unizar.es>
2022-05-11 19:27:38 +0200

    Add update username form

--- src/app/pages/ajustes-usuario/ajustes-usuario.component.ts
+++ src/app/pages/ajustes-usuario/ajustes-usuario.component.ts
@@ -1,29 +1,76 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from '../../services/user.service';
-
+import {User} from "../../models/User";
+import * as AjustesUsuarioConstants from './ajustes-usuario.constants'
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {FormValidatorService} from "../../services/form-validator.service";
+import {HttpErrorResponse} from "@angular/common/http";
 @Component({
   selector: 'app-ajustes-usuario',
   templateUrl: './ajustes-usuario.component.html',
   styleUrls: ['./ajustes-usuario.component.css']
 })
 export class AjustesUsuarioComponent implements OnInit {
-  user: any;
+  // Constants
+  title:string = AjustesUsuarioConstants.TITLE;
+  usernameLabel:string = AjustesUsuarioConstants.USERNAME_LABEL
+  emailLabel:string = AjustesUsuarioConstants.EMAIL_LABEL
 
-  constructor(public userService: UserService, private router: Router) {}
+  //User info
+  user!: User;
+  // Change username form
+  changeUsernameForm!: FormGroup;
+  // changeUsername form errors
+  changeUsernameFormErrors = {
+    username: undefined,
+  };
+  // Server error flags
+  userAlreadyExists:boolean = false
+
+  constructor(public userService: UserService,
+              private router: Router,
+              private fb: FormBuilder,
+              private validator: FormValidatorService
+  ) {}
 
   ngOnInit(): void {
-    this.userService.getProfile().subscribe(
-      (res: any) => {
-        this.user = res['user'];
-      },
-      (err) => {}
-    );
-    console.log(this.user);
+    // Get user profile from backend
+    // this.userService.getProfile().subscribe((user: User) => {this.user = user});
+
+    // Configure changeUsername form validators
+    this.changeUsernameForm = this.fb.group({
+      // username field cannot be empty
+      username: ['', [Validators.required]],
+    });
+
+    // Subscribe changeUsername form onValueChange event
+    this.changeUsernameForm.valueChanges.subscribe(() => {this.updateChangeUsernameErrors()});
+
   }
 
-  onLogout() {
-    this.userService.unSetToken();
-    this.router.navigateByUrl('/login');
+  // Get errors and update form
+  updateChangeUsernameErrors() {
+    this.changeUsernameFormErrors = this.validator.getValidationErrors(this.changeUsernameForm, AjustesUsuarioConstants.VALIDATION_MESSAGES);
   }
+
+  onChangeUsernameSubmit() {
+    // Check errors
+    if (!this.changeUsernameForm.valid) {
+      this.updateChangeUsernameErrors();
+      return;
+    }
+
+    // Submit action
+    this.userService.changeUsername(this.changeUsernameForm.value).subscribe(
+      (res: any) => { this.userAlreadyExists = false },
+      (error) => {
+        if (error.status === 404) {
+          // username already exists
+          this.userAlreadyExists = true
+        } else throw new HttpErrorResponse(error) //GlobalErrorHandler will handle it
+      }
+    );
+  }
+
 }
---
+++ src/app/pages/ajustes-usuario/ajustes-usuario.constants.ts
@@ -0,0 +1,17 @@
+export const TITLE:string = "Ajustes usuario";
+export const USERNAME_LABEL:string = "Usuario";
+export const EMAIL_LABEL:string = "Correo";
+export const ACTUAL_PASSWORD_LABEL:string = "Contraseña actual";
+export const NEW_PASSWORD_LABEL:string = "Nueva contraseña";
+export const USERNAME_BUTTON:string = "Actualizar usuario";
+export const PASSWORD_BUTTON:string = "Actualizar contraseña";
+
+export const VALIDATION_MESSAGES = {
+  'username': {
+    'required': 'Por favor, introduce un usuario',
+  },
+};
+
+
+
+
